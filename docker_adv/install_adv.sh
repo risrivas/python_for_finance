803648182cc19c1f40c62e1ae336b38c7f2af381
Rishi Srivastava <rishisrivasta@gmail.com>
2021-11-28 23:55:22 +0800

    basic data types

---
+++ docker_adv/install_adv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y bzip2 gcc git htop screen vim wget
+apt-get upgrade -y bash
+apt-get clean
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh
+bash Miniconda.sh -b
+rm -rf Miniconda.sh
+
+export PATH="/root/miniconda3/bin:$PATH"
+
+conda update -y conda python
+conda install -y jupyter ipython pytables pandas matplotlib scikit-learn openpyxl pyyaml
+
