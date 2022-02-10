f0e1575bfb2e3f13874c0bb62601e91f24d46fb4
Rishi Srivastava <rishisrivasta@gmail.com>
2022-02-10 14:03:27 +0800

    historical bars

---
+++ alpaca_revisited/api_hist.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 09:28:59 2022
+
+@author: tanvimohan
+"""
+
+import requests
+import os
+import json
+
+os.chdir("/Users/tanvimohan/Desktop/algo_trading/python_for_finance/alpaca_revisited")
+
+
+endpoint = "https://data.alpaca.markets/v2"
+headers = json.loads(open("key.txt", 'r').read())
+
+
+symbol = "XLRE"
+params = {
+    "start": "2022-02-08T14:01:00Z",
+    "limit": 200,
+    # "end" : "2022-02-08T16:01:00Z",
+    "timeframe": "15Min"
+}
+
+# single bar for single symbol
+bar_url = endpoint + f"/stocks/{symbol}/bars"
+
+r = requests.get(bar_url, headers=headers, params=params)
+
+# r.text
+data = r.json()
